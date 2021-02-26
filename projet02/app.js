2e5fb1bcf17a890685172408e3a590aa1fc0150d
LAMRI Rayane <34587541+RayaneLamri@users.noreply.github.com>
2021-02-26 10:36:27 +0100

    Update app.js

--- projet02/app.js
+++ projet02/app.js
@@ -1,51 +1,46 @@
 const txt = document.querySelector(".todo-text");
 const list = document.querySelector(".todo-list");
+let keys = Object.keys(localStorage);
 
 document.querySelector(".todo-submit").addEventListener("click", (e) => {
   e.preventDefault();
-  const num = Date.now();
-  list.innerHTML +=
-    "<div data-num='" +
-    num +
-    "' class='todo-item'><li onblur='blurFunction(event)'>" +
-    txt.value +
-    "</li><button class='button todo-edit' onclick='editFunction(event)'>Edit</button><button onclick='deleteItem(event)' class='button todo-delete'>X</button></div>";
-  localStorage.setItem(num, txt.value);
+  const num = Date.now()
+  const div = document.createElement("div");
+  div.setAttribute("data-num",num);
+  div.setAttribute("class","todo-item");  
+  div.innerHTML += "<li onblur='blurFunction(event)'>" + txt.value + "</li><button class='button todo-edit' onclick='editFunction(event)'>Edit</button><button onclick='deleteItem(event)' class='button todo-delete'>X</button>";
+  list.insertBefore(div, list.childNodes[0]);
+  localStorage.setItem(num,txt.value);
   txt.value = "";
 });
 
 function deleteItem(event) {
-  if (event.target.className == "button todo-delete") {
+  if(event.target.className == "button todo-delete") {
     event.target.parentElement.remove();
-    localStorage.removeItem(
-      event.target.parentElement.getAttribute("data-num")
-    );
-  }
+    localStorage.removeItem(event.target.parentElement.getAttribute("data-num"));
+  } 
 }
 
 function editFunction(event) {
-  event.target.previousSibling.setAttribute("contenteditable", "true");
+  event.target.previousSibling.setAttribute("contenteditable","true");
   event.target.previousSibling.focus();
 }
 
 function blurFunction(event) {
   event.target.removeAttribute("contenteditable");
-  localStorage.setItem(
-    event.target.parentElement.getAttribute("data-num"),
-    event.target.innerText
-  );
+  localStorage.setItem(event.target.parentElement.getAttribute("data-num"),event.target.innerText);
 }
 
 function back() {
-  if(localStorage.length !== 0){
-    for (var key in localStorage) {
-      const div = document.createElement("div");
-      div.setAttribute("data-num",key);
-      div.setAttribute("class","todo-item");  
-      div.innerHTML += "<li onblur='blurFunction(event)'>" + localStorage[key] + "</li><button class='button todo-edit' onclick='editFunction(event)'>Edit</button><button onclick='deleteItem(event)' class='button todo-delete'>X</button>";
-      list.insertBefore(div, list.childNodes[0]);
-    }
+  //if(keys.length !== 0){    
+  for (let key of keys) {
+    const div = document.createElement("div");
+    div.setAttribute("data-num",key);
+    div.setAttribute("class","todo-item");  
+    div.innerHTML += "<li onblur='blurFunction(event)'>" + localStorage[key] + "</li><button class='button todo-edit' onclick='editFunction(event)'>Edit</button><button onclick='deleteItem(event)' class='button todo-delete'>X</button>";
+    list.appendChild(div);
   }
 };
+//};
 
 back();
