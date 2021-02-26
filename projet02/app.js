2f3466afd7d8ada63c6fa5fca16046ec17d1d415
LAMRI Rayane <34587541+RayaneLamri@users.noreply.github.com>
2021-02-26 10:01:41 +0100

    Update app.js

--- projet02/app.js
+++ projet02/app.js
@@ -35,3 +35,17 @@ function blurFunction(event) {
     event.target.innerText
   );
 }
+
+function back() {
+  if(localStorage.length !== 0){
+    for (var key in localStorage) {
+      const div = document.createElement("div");
+      div.setAttribute("data-num",key);
+      div.setAttribute("class","todo-item");  
+      div.innerHTML += "<li onblur='blurFunction(event)'>" + localStorage[key] + "</li><button class='button todo-edit' onclick='editFunction(event)'>Edit</button><button onclick='deleteItem(event)' class='button todo-delete'>X</button>";
+      list.insertBefore(div, list.childNodes[0]);
+    }
+  }
+};
+
+back();
